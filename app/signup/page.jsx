b74377309e0e703214d197896ec2a401e86408f2
yash kathoke <kathokeyash68@gmail.com>
2024-12-12 19:22:57 +0530

    last commit

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -83,7 +83,7 @@ export default function SignUp() {
                   value={name}
                   onChange={(e) => setName(e.target.value)}
                   className="appearance-none block w-full pl-10 px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                  placeholder="John Doe"
+                  placeholder="Raj Kapoor"
                 />
               </div>
             </div>
